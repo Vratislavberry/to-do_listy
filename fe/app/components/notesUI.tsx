6f8a7e5e192dd0ef453f822898b54b08d7fdacda
Vratislav Beran <beran.vratislav23@gmail.com>
2025-07-08 10:40:42 +0200

    Deleted unused code, added basic styling to notes

--- fe/app/components/notesUI.tsx
+++ fe/app/components/notesUI.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect, useContext } from "react";
 import FetchHelper from "../fetchHelper";
-import { Row, Col, Button } from "react-bootstrap";
+import { Row, Col, Button, ButtonGroup } from "react-bootstrap";
 import { Note, NoteDto } from "../types";
 
 import { NoteListContext } from "./notesProvider";
@@ -10,130 +10,23 @@ import PendingItem from "./pendingItem";
 const NotesUI = () => {
   const context = useContext(NoteListContext);
   const { state, data, handlerMap } = context ?? { state: "pending", data: [] };
-  const [noteList, setNoteList] = useState<Note[]>([
-    {
-      id: "1",
-      title: "temp",
-      createdAt: "2023-10-01T12:00:00Z",
-      updatedAt: "2023-10-01T12:00:00Z",
-    },
-    {
-      id: "2",
-      title: "dummy",
-      createdAt: "2023-10-02T12:00:00Z",
-      updatedAt: "2023-10-02T12:00:00Z",
-    },
-  ]);
-
-  const getNotes = async () => {
-    const result = await FetchHelper.note.list();
-    if (result.ok) {
-      setNoteList(result.data); // Update state with fetched notes
-    } else {
-      console.error("Error:", result.status);
-    }
-  };
-
-  const createNote = async (dtoIn: NoteDto) => {
-    const result = await FetchHelper.note.create(dtoIn);
-    if (result.ok) {
-      // Append new note to the list
-      setNoteList((current) => [...current, result.data]);
-    } else {
-      console.error("Error creating note:", result.status);
-    }
-  };
-
-  const updateNote = async (dtoIn: NoteDto) => {
-    const result = await FetchHelper.note.update(dtoIn);
-    if (result.ok) {
-      // Update the note in the list
-      setNoteList((current) =>
-        current.map((note) => (note.id === dtoIn?.id ? result.data : note))
-      );
-    } else {
-      console.error("Error updating note:", result.status);
-    }
-  };
-
-  const deleteNote = async (dtoIn: NoteDto) => {
-    const result = await FetchHelper.note.delete(dtoIn);
-    if (result.ok) {
-      // Remove the note from the list
-      setNoteList((current) => current.filter((note) => note.id !== dtoIn.id));
-    } else {
-      console.error("Error deleting note:", result.status);
-    }
-  };
-
-  useEffect(() => {
-    getNotes();
-  }, []);
 
   return (
     <>
       <Row>
-        {noteList.map((note) => (
-          <Col sm="4" key={note.id}>
-            {note.title}
-          </Col>
-        ))}
-
-        <Button
-          onClick={() => {
-            createNote({
-              title: "Created Note",
-              createdAt: "2023-10-02T12:00:00Z",
-              updatedAt: "2023-10-02T12:00:00Z",
-            });
-          }}
-        >
-          Create
-        </Button>
-
-        <Button
-          onClick={() => {
-            updateNote({
-              id: "408c",
-              title: "Updated Note 2",
-              createdAt: "2023-10-02T12:00:00Z",
-              updatedAt: "2023-10-02T12:00:00Z",
-            });
-          }}
-        >
-          Update
-        </Button>
-
-        <Button
-          onClick={() => {
-            deleteNote({
-              id: "f05f",
-              title: "Updated Note 2",
-              createdAt: "2023-10-02T12:00:00Z",
-              updatedAt: "2023-10-02T12:00:00Z",
-            });
-          }}
-        >
-          Delete
-        </Button>
-
-        <Button
-          onClick={() => {
-            console.log("Logging data:", state, data);
-          }}
-        >
-          log data
-        </Button>
-      </Row>
-
-      <Row>
-        <p>Nová data:</p>
         {state === "pending" ? <PendingItem /> : null}
 
         {state === "ready"
           ? data?.map((note) => (
-              <Col sm="4" key={note.id}>
-                {note.title}
+              <Col
+                sm="12"
+                className="d-flex justify-content-center text-center my-2"
+                key={note.id}
+              >
+                <ButtonGroup key={note.id} className="w-100">
+                  <Button className="flex-grow-1">{note.title}</Button>
+                  <Button className="flex-grow-0">Check</Button>
+                </ButtonGroup>
               </Col>
             ))
           : null}
@@ -153,8 +46,8 @@ const NotesUI = () => {
         <Button
           onClick={() => {
             handlerMap?.handleUpdate({
-              id: "8492",
-              title: "Updated note",
+              id: "a051",
+              title: "another one",
               createdAt: "2023-10-02T12:00:00Z",
               updatedAt: "2023-10-02T12:00:00Z",
             });
@@ -166,7 +59,7 @@ const NotesUI = () => {
         <Button
           onClick={() => {
             handlerMap?.handleDelete({
-              id: "408c",
+              id: "a051",
               title: "Updated note",
               createdAt: "2023-10-02T12:00:00Z",
               updatedAt: "2023-10-02T12:00:00Z",
