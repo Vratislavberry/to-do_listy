b2cc0332bfa945c30eccbdf48b55b275151e299d
Vratislav Beran <beran.vratislav23@gmail.com>
2025-07-08 11:24:48 +0200

    Added addNote form (nonfunctional)

---
+++ fe/app/components/NoteForm.jsx
@@ -0,0 +1,82 @@
+import { useContext, useState } from "react";
+
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+
+import { NoteListContext } from "./notesProvider";
+
+
+function NoteForm({ item, onClose }) {
+  const { state, handlerMap } = useContext(NoteListContext);
+  const [errorState, setErrorState] = useState();
+
+  return (
+    <Modal show={true} onHide={onClose}>
+      <Form
+        onSubmit={async (e) => {
+          // Prevents reloading the page - we handle submit ourselves
+          e.preventDefault();
+          // stops propagation of submit event to parent components
+          // in some cases it can trigger another submit event.
+          e.stopPropagation();
+          const formData = new FormData(e.target);
+          // extracts data from Modal form
+          const values = Object.fromEntries(formData);
+          let result = null;
+          if (item.id){
+            result = await handlerMap.handleUpdate({...values, id: item.id});
+          }
+          else{
+            result = await handlerMap.handleCreate({ ...values });
+          }
+
+          if (result.ok) {
+            onClose();
+          } else {
+            setErrorState(result.error)
+          }
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{item.id ? "Edit" : "Create"} Note</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Label>Title *</Form.Label>
+          <Form.Control
+            type="text"
+            name="title"
+            disabled={state === "pending"}
+            defaultValue={item.title}
+            required
+            maxLength={50}
+          />
+
+          {!!errorState?.note?.message ? (
+          <Alert variant={"danger"}>{errorState.note.message}</Alert>
+        ) : null}
+        
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={onClose}
+            disabled={state === "pending"}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="primary"
+            type="submit"
+            disabled={state === "pending"}
+          >
+            {item.id ? "Edit" : "Create"}
+          </Button>
+        </Modal.Footer>
+      </Form>
+    </Modal>
+  );
+}
+
+export default NoteForm;
--- fe/app/components/notesUI.tsx
+++ fe/app/components/notesUI.tsx
@@ -6,17 +6,35 @@ import { Note, NoteDto } from "../types";
 
 import { NoteListContext } from "./notesProvider";
 import PendingItem from "./pendingItem";
+import NoteForm from "./NoteForm";
 
 const NotesUI = () => {
   const context = useContext(NoteListContext);
   const { state, data, handlerMap } = context ?? { state: "pending", data: [] };
+  const [noteFormData, setNoteFormData] = useState<NoteDto | undefined>(
+    undefined
+  );
 
   return (
     <>
       <Row>
+        {!!noteFormData ? (
+          <NoteForm
+            item={noteFormData}
+            onClose={() => setNoteFormData(undefined)}
+          />
+        ) : null}
+
         <h1 className="text-center my-3">Notes</h1>
         <Col sm="12" className="my-2">
-        <Button variant="success" className="w-100 text-start">Create new note</Button>
+          <Button
+            variant="success"
+            className="w-100 text-start"
+            disabled={(state === "pending")}
+            onClick={() => setNoteFormData({})}
+          >
+            Create new note
+          </Button>
         </Col>
         {state === "pending" ? <PendingItem /> : null}
 
@@ -28,7 +46,9 @@ const NotesUI = () => {
                 key={note.id}
               >
                 <ButtonGroup key={note.id} className="w-100">
-                  <Button className="flex-grow-1 text-start">{note.title}</Button>
+                  <Button className="flex-grow-1 text-start">
+                    {note.title}
+                  </Button>
                   <Button className="flex-grow-0">Check</Button>
                 </ButtonGroup>
               </Col>
